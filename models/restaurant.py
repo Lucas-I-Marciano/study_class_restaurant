df62e8b2457c467537c5f8c170efd7d78be07ce5
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-19 17:10:59 -0300

    :sparkles: feat: Utilizing constructor
    Using by change __init__ function

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,16 +1,16 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __init__(self, nome, categoria) :
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
 
-restaurante_sushi = Restaurante()
-restaurante_sushi.nome = 'Sushi'
-restaurante_sushi.categoria = 'japonesa'
+restaurante_sushi = Restaurante('Sushi', 'japonesa')
+print('\n',restaurante_sushi.nome)
+
+restaurante_praca = Restaurante('Praca', 'italiana')
+print('\n',restaurante_praca.nome) 
 
 print('\ndir: ', dir(restaurante_sushi))
 print('\nvars: ', vars(restaurante_sushi))
-
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praca'
-restaurante_praca.categoria = 'italiana'
+print('\nrestaurante_sushi', restaurante_sushi)
