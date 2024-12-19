0c1ece11c1d3675530d6744a03e3e69272b7c23c
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-19 19:37:27 -0300

    :sparkles: feat: Relationing 'Avaliacoes' class with 'Restaurante' class
    Using @property to make it less verbose

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,9 +1,12 @@
+from models.avaliacoes import Avaliacoes
+
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria) :
         self._nome = nome
         self._categoria = categoria
         self._ativo = False
+        self._avaliacoes = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self) :
@@ -14,13 +17,26 @@ class Restaurante:
 
     @classmethod
     def lista_restaurantes(cls) :
-        print('-'*60)
-        print('Nome'.ljust(20), "|", "Categoria".ljust(20), "|", "Status")
-        print('-'*60)
+        linha = '*'*90
+        print(linha)
+        print('Nome'.ljust(20), "|", "Categoria".ljust(20), "|", "Avaliações".ljust(20), "|", "Status")
+        print(linha)
         for restaurante in cls.restaurantes :
-            print(restaurante._nome.ljust(20), "|", restaurante._categoria.ljust(20), "|", restaurante.ativo)
+            print(restaurante._nome.ljust(20), "|", restaurante._categoria.ljust(20), "|", str(restaurante.calcula_media_avaliacoes).ljust(20), "|", restaurante.ativo)
 
     @property
     def ativo(self):
         return "☑" if self._ativo else "☐"
+
+    def adiciona_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacoes(cliente, nota)
+        self._avaliacoes.append(avaliacao)
+
+    @property
+    def calcula_media_avaliacoes(self):
+        if not self._avaliacoes:
+            return 0
+        soma_das_notas = sum([avaliacao._nota for avaliacao in self._avaliacoes])
+        total_de_notas = len(self._avaliacoes)
+        return round(soma_das_notas / total_de_notas, 1)
 
