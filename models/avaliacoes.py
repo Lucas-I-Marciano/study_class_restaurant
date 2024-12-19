b23080179214ecb58e7f5adfc17cc832061f13ca
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-19 20:39:13 -0300

    :sparkles: feat: Adding docstring to avaliacoes model

--- models/avaliacoes.py
+++ models/avaliacoes.py
@@ -1,4 +1,10 @@
 class Avaliacoes:
     def __init__(self, cliente, nota):
+        """Inicializa uma instância da classe Avaliações
+
+        Args:
+            cliente (string): Nome do cliente que está avaliando
+            nota (float): Nota que o cliente deu para o restaurante
+        """        
         self._cliente = cliente
         self._nota = nota
