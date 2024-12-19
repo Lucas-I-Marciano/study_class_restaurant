424cc144dfad65d5bad9ea702b4acd2880e43739
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-19 19:38:04 -0300

    :sparkles: feat: Adding rates to some restaurants

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ restaurante_brooks = Restaurante("Brooks", "lanche")
 restaurante_kfc = Restaurante("KFC", "fast-food")
 restaurante_sujinho = Restaurante("Sujinho", "brasileira")
 
+restaurante_kanpek.alterna_status()
+restaurante_kanpek.adiciona_avaliacao("Lucas", 10)
+restaurante_kanpek.adiciona_avaliacao("Thaina", 10)
+restaurante_kanpek.adiciona_avaliacao("Adriana", 7)
+
+
 def main():
     Restaurante.lista_restaurantes()
     
