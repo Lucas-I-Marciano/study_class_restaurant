0b9ddeadb481c732110ff981e2c4aadfdeadb524
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-19 17:18:46 -0300

    :sparkles: feat: Creating my own function
    This function will not be able to be called by a instance of the class, as it did not take self as argument

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,11 +1,20 @@
 class Restaurante:
+    restaurantes = []
     def __init__(self, nome, categoria) :
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self) :
         return f'{self.nome} | {self.categoria} | {self.ativo}'
+
+    def lista_restaurantes() :
+        print('-'*30)
+        print('nome'.ljust(20), "|", "categoria")
+        print('-'*30)
+        for restaurante in Restaurante.restaurantes :
+            print(restaurante.nome.ljust(20), "|", restaurante.categoria)
 
 
 restaurante_sushi = Restaurante('Sushi', 'japonesa')
@@ -17,3 +26,6 @@ print('\n',restaurante_praca.nome)
 print('\ndir: ', dir(restaurante_sushi))
 print('\nvars: ', vars(restaurante_sushi))
 print('\nrestaurante_sushi:', restaurante_sushi)
+
+Restaurante.lista_restaurantes()
+
