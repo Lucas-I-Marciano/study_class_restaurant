7d804868c49dea1417a69653b500bfd003e842e0
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-20 10:03:04 -0300

    :sparkles: feat: Adding 'ItemBebida' class as a inheritance of 'ItemCardapio' class
    It will handle with drink menu

---
+++ models/cardapio/itemBebida.py
@@ -0,0 +1,9 @@
+from models.cardapio.itemCardapio import ItemCardapio
+
+class ItemBebida(ItemCardapio):
+    def __init__(self, nome, preco, tamanho) :
+        super().__init__(nome, preco)
+        self._tamanho = tamanho
+
+    def __str__(self):
+        return f'{self._nome.ljust(20)} : R$:{str(self._preco).ljust(5)} | {self._tamanho}'
